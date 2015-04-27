f6629d28c3ceb47cabad2e64d97df09d298238d5
lablear <nicolaj.hjort@gmail.com>
2015-04-27 12:30:04 +0200

    Testing google graph
    changing to include the basic code from google for a pie chat and
    changed to bar chart.

--- index.php
+++ index.php
@@ -1,16 +1,60 @@
+
+<!-- select DOCTYPE for browsers-->
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
+    <head>  
+        <!-- standard header stuff -->
         <meta charset="UTF-8">
-        <title>Test</title>
+        <title>php graph - shoes</title>
+        <!--Load the AJAX API-->
+        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+
+        <!-- change to an include filed instead? -->
+        <script type="text/javascript">
+
+            // Load the Visualization API and the piechart package.
+            google.load('visualization', '1.0', {'packages': ['corechart']});
+
+            // Set a callback to run when the Google Visualization API is loaded.
+            google.setOnLoadCallback(drawChart);
+
+            // Callback that creates and populates a data table,
+            // instantiates the pie chart, passes in the data and
+            // draws it.
+            function drawChart() {
+
+                // Create the data table.
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'Topping');
+                data.addColumn('number', 'Slices');
+                data.addRows([
+                    ['Mushrooms', 3],
+                    ['Onions', 1],
+                    ['Olives', 1],
+                    ['Zucchini', 1],
+                    ['Pepperoni', 2]
+                ]);
+
+                // Set chart options
+                var options = {'title': 'How Much Pizza I Ate Last Night',
+                    'width': 400,
+                    'height': 300};
+
+                // Instantiate and draw our chart, passing in some options.
+                var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+                chart.draw(data, options);
+            }
+        </script>
     </head>
     <body>
-        <h1>Well.... hello there sexy!</h1>
-           
+
+
+        <!--Div that will hold the pie chart-->
+        <div id="chart_div"></div>
+
     </body>
 </html>
+
+
+<!-- Convert the pie chart to a bar chart by replacing 
+google.visualization.PieChart with google.visualization.BarChart in the code and reloading your browser. -->
