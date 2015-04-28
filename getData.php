1e4e1d106e0fa0de54af46f9ab108b22892d9460
lablear <nicolaj.hjort@gmail.com>
2015-04-28 08:51:29 +0200

    testing branch

--- getData.php
+++ getData.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // This is just an example of reading server side data and sending it to the client.
 // It reads a json formatted text file and outputs it.
@@ -7,5 +7,7 @@ $string = file_get_contents("sampleData.json");
 echo $string;
 
 // query your database and parse into JSON etc etc
+
+// test af branch test 1
 
 ?>
