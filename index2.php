ab8ba229d6b626d0cabfe13fe32bdd80ee1d3387
lablear <nicolaj.hjort@gmail.com>
2015-04-29 11:03:00 +0200

    small changes
    ..

--- index2.php
+++ index2.php
@@ -1,40 +1,36 @@
 <html>
-  <head>
-    <!--Load the AJAX API-->
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script type="text/javascript">
+    <head>
 
-    // Load the Visualization API and the piechart package.
-    google.load('visualization', '1', {'packages':['corechart']});
+        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script type="text/javascript">
+            // Load the Visualization API and the piechart package.
+            google.load('visualization', '1', {'packages': ['corechart']});
+            
+            // Set a callback to run when the Google Visualization API is loaded.
+            google.setOnLoadCallback(drawChart);
 
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.setOnLoadCallback(drawChart);
+            function drawChart() {
+                var jsonData = $.ajax({
+                    url: "getData.php",
+                    dataType: "json",
+                    async: false
+                }).responseText;
 
-    function drawChart() {
-      var jsonData = $.ajax({
-          url: "getData.php",
-          dataType:"json",
-          async: false
-          }).responseText;
+                // Create our data table out of JSON data loaded from server.
+                var data = new google.visualization.DataTable(jsonData);
 
-      // Create our data table out of JSON data loaded from server.
-      var data = new google.visualization.DataTable(jsonData);
+                // Instantiate and draw our chart, passing in some options.
+                var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+                chart.draw(data, {width: 400, height: 240});
+            }
+        </script>
 
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-      chart.draw(data, {width: 400, height: 240});
-    }
-    </script>
-
-  </head>
-
-  <body>
-      
-      <h1>Frequence of shoesizes</h1>
-    <!-- div container der holder på chart elementet START-->
-    <div id="chart_div"></div>
-    <!-- Chart div SLUT-->
-  </div>
-  </body>
+    </head>
+    <body>
+        <h1>Frequence of shoesizes</h1>
+        <!-- div container der holder på chart elementet START-->
+        <div id="chart_div"></div>
+        <!-- Chart div SLUT-->
+    </body>
 </html>
